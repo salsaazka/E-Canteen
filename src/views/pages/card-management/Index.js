265a73fd9e0d1336dd784af822ae18b93d3643b6
Yogapratama1109 <yoga110011@gmail.com>
2023-09-09 15:34:15 +0700

    update

--- src/views/pages/card-management/Index.js
+++ src/views/pages/card-management/Index.js
@@ -2,23 +2,23 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
-const CardsManagement  = () => {
-    return (
-        <div className="card">
-            <div className="card-body">
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicName">
-                        <Form.Label>Name Cards</Form.Label>
-                        <Form.Control type="text" placeholder="Insert Your Cards" />
-                    </Form.Group>
+const CardsManagement = () => {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Name Cards</Form.Label>
+            <Form.Control type="text" placeholder="Insert Your Cards" />
+          </Form.Group>
 
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </div>
-        </div>
-    )
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
+  )
 }
 
 export default CardsManagement
