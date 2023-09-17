009d7708e8683c283f923dc48003a3e8f4234e13
Yogapratama1109 <yoga110011@gmail.com>
2023-09-17 15:04:39 +0700

    update

--- src/views/pages/device/table.js
+++ src/views/pages/device/table.js
@@ -2,10 +2,12 @@
 import React, { useState, useEffect } from 'react'
 import { useTable } from 'react-table'
 import axios from 'axios'
-import { ToastContainer, toast } from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 const DeviceTable = () => {
   const [device, setDevices] = useState([])
+  const navigate = useNavigate()
 
   const getDevices = () => {
     axios
@@ -65,6 +67,21 @@ const DeviceTable = () => {
         Cell: (props) => {
           return (
             <div className="d-flex justify-content-center align-items-center">
+              <button
+                className="btn btn-primary me-2"
+                onClick={navigate('/devices/edit/' + props.row.original.id)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="#FFFFFF"
+                  className="bi bi-trash3"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                </svg>
+              </button>
               <button
                 className="btn btn-danger"
                 onClick={() => handleDelete(props?.row?.original?.id)}
