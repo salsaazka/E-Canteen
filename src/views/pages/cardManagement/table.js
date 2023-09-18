b96b05ce99daa26a4973e9eee7d7d8666921036f
salsaazka <salsabilaazkaputri@smkwikrama.sch.id>
2023-09-18 12:38:49 +0700

    update card table

--- src/views/pages/cardManagement/table.js
+++ src/views/pages/cardManagement/table.js
@@ -1,15 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useTable } from 'react-table'
+import axios from 'axios'
+import { ToastContainer } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 const CardTable = () => {
- 
+  const [card, setCard] = useState([])
+  const [user, setUser] = useState([])``
+  const navigate = useNavigate()
 
-  const data = listData.map((item, index) => ({
-    id: item.id,
+  const getCard = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/api/v1/card`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        console.log(res.data.data)
+        setCard(res.data.data)
+        setUser(res.data.data)
+      })
+      .catch((err) => {
+        console.log(err.response.data.message)
+      })
+  }
+
+  useEffect(() => {
+    getCard()
+  }, [])
+
+  const data = card.map((item, index) => ({
+    id: item._id,
     number: index + 1,
-    name: item.name,
-    address: item.address,
+    card: item.card_id,
+    user: item.user_id,
   }))
+
+  const handleDelete = (id) => {
+    axios
+      .delete(`${process.env.REACT_APP_API_URL}/api/v1/card/${id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        console.log(res.data.data)
+        getCard()
+      })
+      .catch((err) => {
+        console.log(err.response.data.message)
+      })
+  }
 
   const columns = React.useMemo(
     () => [
@@ -18,18 +60,45 @@ const CardTable = () => {
         accessor: 'number',
       },
       {
-        Header: 'Name',
-        accessor: 'name',
+        Header: 'Card ID',
+        accessor: 'card',
       },
       {
-        Header: 'Address',
-        accessor: 'address',
+        Header: 'User ID',
+        accessor: 'user',
       },
       {
         Header: 'Action',
         accessor: 'action',
         Cell: (props) => {
-          return <></>
+          return (
+            <div className="d-flex justify-content-center align-items-center">
+              <button className="btn btn-primary me-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-pencil"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                </svg>
+              </button>
+              <button className="btn btn-danger">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="#FFFFFF"
+                  className="bi bi-trash3"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                </svg>
+              </button>
+            </div>
+          )
         },
       },
     ],
@@ -43,7 +112,7 @@ const CardTable = () => {
 
   return (
     <div>
-      <table {...getTableProps()} className="w-100 bg-light">
+      <table {...getTableProps()} className="w-100 bg-light rounded mt-3">
         <thead className="text-left">
           {headerGroups.map((headerGroup) => (
             <tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
@@ -59,7 +128,11 @@ const CardTable = () => {
           {rows.map((row) => {
             prepareRow(row)
             return (
-              <tr key={row.id} {...row.getRowProps()}>
+              <tr
+                key={row.id}
+                {...row.getRowProps()}
+                style={{ backgroundColor: '#fff', borderBottom: '1px solid #D9D9D9' }}
+              >
                 {row.cells.map((cell) => {
                   return (
                     <td key={cell.column.id} {...cell.getCellProps()} className="border-none p-3">
@@ -72,6 +145,7 @@ const CardTable = () => {
           })}
         </tbody>
       </table>
+      <ToastContainer />
     </div>
   )
 }
