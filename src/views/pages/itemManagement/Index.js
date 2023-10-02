31c9545c1a6a4ca410dd9f3a43c1f4043515ffcf
Fadhli576 <91299096+Fadhli576@users.noreply.github.com>
2023-10-02 09:14:41 +0700

    fix search

--- src/views/pages/itemManagement/Index.js
+++ src/views/pages/itemManagement/Index.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import ItemsTable from './table'
+import { cilZoom } from '@coreui/icons'
 import { useNavigate } from 'react-router-dom'
+import CIcon from '@coreui/icons-react'
+import { CButton } from '@coreui/react'
+
 const Items = () => {
   const navigate = useNavigate()
   const handleNavigate = () => {
@@ -15,13 +19,18 @@ const Items = () => {
     <div className="card">
       <div className="card-body">
         <div className="d-flex justify-content-between">
-          <Form.Group className="w-75" controlId="formBasicName">
+          <Form.Group className="mb-3 d-flex border" controlId="formBasicName">
             <Form.Control
               id="nameItem"
               onChange={(e) => setFilter(e.target.value)}
               type="text"
+              className="border-0"
               placeholder="Insert Your Items"
             />
+            <CButton color="white" className="">
+              {' '}
+              <CIcon icon={cilZoom} customClassName="" />
+            </CButton>
           </Form.Group>
           <Button variant="primary" onClick={handleNavigate}>
             Add Item
--- src/views/pages/userManagement/Index.js
+++ src/views/pages/userManagement/Index.js
@@ -1,21 +1,28 @@
 import React, { useState } from 'react'
-import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import UserManagementTable from './table'
+import CIcon from '@coreui/icons-react'
+import { CButton } from '@coreui/react'
+import { cilZoom } from '@coreui/icons'
 
 const UserManagement = () => {
   const [filter, setFilter] = useState('')
   return (
     <div className="card">
       <div className="card-body">
-        <div className="d-flex justify-content-center">
-          <Form.Group className="w-100 mb-3" controlId="formBasicName">
+        <div className="d-flex">
+          <Form.Group className="mb-3 d-flex border" controlId="formBasicName">
             <Form.Control
               id="nameItem"
               onChange={(e) => setFilter(e.target.value)}
               type="text"
+              className="border-0"
               placeholder="Insert Your Name"
             />
+            <CButton color="white" className="">
+              {' '}
+              <CIcon icon={cilZoom} customClassName="" />
+            </CButton>
           </Form.Group>
         </div>
         <UserManagementTable filter={filter} />
